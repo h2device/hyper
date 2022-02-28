601a4df51629b5b5a5113f0214f08cd856ddd7b1
naveen-vivek <vivek@purdue.edu>
2022-02-27 19:16:11 -0500

    First commit

---
+++ adc/main/battery.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   64          //Multisampling
+#define BATTERY_LIFE(v)   (-811.88*v*v) + (3161.8*v) - (1707.4)
+#define BATTERY_CONVERT_TO_UNITS(val)    (1320 - ((float) val))/(float)13.2
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC2_CHANNEL_4;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_11db;
+static const adc_unit_t unit = ADC_UNIT_2;
+
+static void check_efuse(void) {
+    //Check if TP is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+
+static void print_char_val_type(esp_adc_cal_value_t val_type) {
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+
+void app_main(void) {
+    
+    check_efuse(); // check if Two Point or Vref are burned into eFuse
+
+    // Configure ADC
+    if (unit == ADC_UNIT_1) {
+        adc1_config_width(width);
+        adc1_config_channel_atten(channel, atten);
+    } 
+    if (unit == ADC_UNIT_2) {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    // Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
+    // Continuously sample ADC1
+    while (1) {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            if (unit == ADC_UNIT_1) {
+                adc_reading += adc1_get_raw((adc1_channel_t)channel);
+            }
+            if (unit == ADC_UNIT_2) {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, width, &raw);
+                adc_reading += raw;
+            }
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\t", adc_reading, voltage);
+        printf("Battery remaining: %f %%\n", BATTERY_CONVERT_TO_UNITS(BATTERY_LIFE( (float) (voltage) / 1000 )));
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
