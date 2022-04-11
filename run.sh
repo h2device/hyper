2ee0aaf40f49b7ad20462fe38fb82f5fc4536924
naveen-vivek <vivek@purdue.edu>
2022-04-11 15:22:30 -0400

    Working adc, mpu

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 . $HOME/esp/esp-idf/export.sh
 idf.py build
-idf.py -p /dev/tty.SLAB_USBtoUART flash
-# idf.py -p /dev/tty.usbserial-0001 flash
-screen /dev/tty.SLAB_USBtoUART 115200
+idf.py -p /dev/tty.SLAB_USBtoUART flash monitor
+idf.py -p /dev/tty.usbserial-0001 flash monitor
+idf.py -p /dev/cu.usbserial-A50285BI flash monitor
