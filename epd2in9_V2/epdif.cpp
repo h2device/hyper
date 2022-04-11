9791530f5c3ecde924c29976c194acefce6f383d
2heen <tuhinsarkar484@gmail.com>
2022-04-11 11:52:38 -0400

    changed epdif.cpp, ready for testing

--- epd2in9_V2/epdif.cpp
+++ epd2in9_V2/epdif.cpp
@@ -1,4 +1,4 @@
-/**
+q/**
  *  @filename   :   epdif.cpp
  *  @brief      :   Implements EPD interface functions
  *                  Users have to implement all the functions in epdif.cpp
@@ -33,6 +33,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/spi_master.h"
+#include "driver/spi_common.h"
 #include "driver/gpio.h"
 
 #include "sdkconfig.h"
@@ -88,17 +89,7 @@ int EpdIf::IfInit(void) {
     SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
     return 0;
     */
-    send_lines(); // NEED TO PASS IN PARAMETESR HERE
-    spi_bus_config_t buscfg={
-        .miso_io_num = PIN_NUM_MISO,
-        .mosi_io_num = PIN_NUM_MOSI,
-        .sclk_io_num = PIN_NUM_CLK,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = 32,
-    };
-    //Initialize the SPI bus
-    ret = spi_bus_initialize(EEPROM_HOST, &buscfg, SPI_DMA_CH_AUTO);
+
     // SPI.begin();
     gpio_set_direction(MOSI_PIN, GPIO_MODE_OUTPUT);
     vTaskDelay(100 / portTICK_RATE_MS);
@@ -106,45 +97,27 @@ int EpdIf::IfInit(void) {
     vTaskDelay(100 / portTICK_RATE_MS);
     gpio_set_direction(MISO_PIN, GPIO_MODE_OUTPUT);
     vTaskDelay(100 / portTICK_RATE_MS);
-    // SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
-    spi_bus_config_t buscfg = {
-		.mosi_io_num = DATA_PIN,
-		.miso_io_num = 5,
-		.sclk_io_num = CLK_PIN,
-		.quadwp_io_num = -1,
-		.quadhd_io_num = -1,
-		.max_transfer_sz = SPI_MAX_DMA_LEN,
-	};
-    // Configuration for the SPI master interface
-	spi_device_interface_config_t devcfg = {
-		.command_bits = 0,
-		.address_bits = 0,
-		.dummy_bits = 0,
-		.duty_cycle_pos = 128,
-		.cs_ena_pretrans = 0,
-		.cs_ena_posttrans = 0,
-		.clock_speed_hz = 26*1000*1000,
-		.mode = 0,
-		.spics_io_num = 17,
-		.queue_size = 1,
-		.flags = 0,
-		.pre_cb = NULL,
-		.post_cb = NULL,
-	};
-
-    spi_bus_initialize(VSPI_HOST, &buscfg, 1);
-	spi_bus_add_device(VSPI_HOST, &devcfg, &spi_handle);
-
-    /*
-    //Send all the commands
-    while (lcd_init_cmds[cmd].databytes!=0xff) {
-        lcd_cmd(spi, lcd_init_cmds[cmd].cmd, false);
-        lcd_data(spi, lcd_init_cmds[cmd].data, lcd_init_cmds[cmd].databytes&0x1F);
-        if (lcd_init_cmds[cmd].databytes&0x80) {
-            vTaskDelay(100 / portTICK_PERIOD_MS);
-        }
-        cmd++;
-    }
-    */
-	
+    
+	// Initialize bus
+    spi_device_handle_t spi;
+    spi_bus_config_t bus_config={
+        .miso_io_num=MISO_PIN,
+        .mosi_io_num=MOSI_PIN,
+        .sclk_io_num=CLK_PIN,
+        .quadwp_io_num=-1,
+        .quadhd_io_num=-1,
+        .max_transfer_sz=4092
+    };
+    spi_dma_chan_t dma_chan = SPI_DMA_CH_AUTO;
+    ret = spi_bus_initialize(VSPI_HOST, bus_config, dma_chan);
+    ESP_ERROR_CHECK(ret);
+    // Adding Eink to bus
+    spi_device_interface_config_t devcfg={
+        .clock_speed_hz=2000000,
+        .mode=0,
+        .spics_io_num=CS_PIN,
+        .queue_size=7
+    };
+    ret=spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
+    ESP_ERROR_CHECK(ret);
 }
