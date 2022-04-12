c0f085dfb1fe9c5a628d358d9ec8a288ab727758
naveen-vivek <vivek@purdue.edu>
2022-04-12 17:56:49 -0400

    Display partially working

--- epd2in9_V2/epdif.cpp
+++ epd2in9_V2/epdif.cpp
@@ -26,7 +26,6 @@ q/**
  */
 
 #include "epdif.h"
-// #include <SPI.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -39,13 +38,11 @@ q/**
 #include "sdkconfig.h"
 #include "esp_log.h"
 
-#define EPD_HOST HSPI_HOST
 
-EpdIf::EpdIf() {
-};
 
-EpdIf::~EpdIf() {
-};
+EpdIf::EpdIf() { public: spi_device_handle_t spi; };
+
+EpdIf::~EpdIf() {};
 
 void EpdIf::DigitalWrite(int pin, int value) {
     // digitalWrite(pin, value); // ARDUINO
@@ -54,9 +51,9 @@ void EpdIf::DigitalWrite(int pin, int value) {
 }
 
 int EpdIf::DigitalRead(int pin) {
+    // return digitalRead(pin); // ARDUINO
     gpio_set_direction(pin, GPIO_MODE_INPUT);
     return gpio_get_level(pin);
-    // return digitalRead(pin); // ARDUINO
 }
 
 void EpdIf::DelayMs(unsigned int delaytime) {
@@ -65,60 +62,32 @@ void EpdIf::DelayMs(unsigned int delaytime) {
 }
 
 void EpdIf::SpiTransfer(unsigned char data) {
-    digitalWrite(CS_PIN, LOW);
-    // TODO: Convert to ESP-IDF
-    digitalWrite(CS_PIN, HIGH);
     /*
     digitalWrite(CS_PIN, LOW);
     SPI.transfer(data);
     digitalWrite(CS_PIN, HIGH);
     */
+
+    digitalWrite(CS_PIN, LOW);
+    // SPI.transfer(data);
+    spi_transaction_t t;
+    memset(&t, 0, sizeof(t));       //Zero out the transaction
+    t.length=8;                     //Command is 8 bits
+    t.tx_buffer=&data;               //The data is the cmd itself
+    t.user=(void*)0;                //D/C needs to be set to 0
+    ret=spi_device_polling_transmit(spi, &t);  //Transmit!
+    assert(ret==ESP_OK);            //Should have had no issues.
+    memset(&t, 0, sizeof(t));
+    t.length=8;
+    t.flags = SPI_TRANS_USE_RXDATA;
+    t.user = (void*)1;
+    ret = spi_device_polling_transmit(spi, &t);
+    assert( ret == ESP_OK );
+
+    digitalWrite(CS_PIN, HIGH);
 }
 
 int EpdIf::IfInit(void) {
-    gpio_set_direction(CS_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(RST_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(DC_PIN, GPIO_MODE_OUTPUT);
-    gpio_set_direction(BUSY_PIN, GPIO_MODE_INPUT);
-    // SPI.begin()
-    esp_err_t err;
-    spi_device_handle_t spi_handle;
-
-    spi_bus_config_t buscfg = {
-        .mosi_io_num = MOSI_PIN,
-        .miso_io_num = MISO_PIN,
-        .sclk_io_num = CLK_PIN,
-    };
-    err = spi_bus_initialize(EPD_HOST, &buscfg, SPI_DMA_CH_AUTO);
-    assert( err == ESP_OK );
-
-    spi_device_interface_config_t devcfg = {
-        .mode = 0,
-        .clock_speed_hz = 500 * 1000,
-        .spics_io_num = CS_PIN,
-        .flags = SPI_TRANS_USE_RXDATA,
-        .queue_size = 1
-    };
-    err = spi_bus_add_device(EPD_HOST, &devcfg, &spi_handle);
-    assert( err == ESP_OK );
-
-    /*
-    uint8_t cmd = 0x03;
-
-    spi_transaction_t t;
-    memset(&t, 0, sizeof(t));
-    t.length = 8;
-    t.tx_buffer = &cmd;
-
-    gpio_set_level(PIN_NUM_SS, 0);
-    err = spi_device_transmit(opcn2_handle, &t);
-    gpio_set_level(PIN_NUM_SS, 1);
-    assert( err == ESP_OK );
-
-    ESP_LOGI(TAG, "response: 0x%02x", t.rx_data[0]);
-    */
-
-    // TODO: Convert to ESP-IDF
     /*
     pinMode(CS_PIN, OUTPUT);
     pinMode(RST_PIN, OUTPUT);
@@ -128,7 +97,10 @@ int EpdIf::IfInit(void) {
     SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
     return 0;
     */
-
+    gpio_set_direction(CS_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(RST_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(DC_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BUSY_PIN, GPIO_MODE_INPUT);
     // SPI.begin();
     gpio_set_direction(MOSI_PIN, GPIO_MODE_OUTPUT);
     vTaskDelay(100 / portTICK_RATE_MS);
@@ -137,8 +109,7 @@ int EpdIf::IfInit(void) {
     gpio_set_direction(MISO_PIN, GPIO_MODE_OUTPUT);
     vTaskDelay(100 / portTICK_RATE_MS);
     
-	// Initialize bus
-    spi_device_handle_t spi;
+	// SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
     spi_bus_config_t bus_config={
         .miso_io_num=MISO_PIN,
         .mosi_io_num=MOSI_PIN,
@@ -150,7 +121,7 @@ int EpdIf::IfInit(void) {
     spi_dma_chan_t dma_chan = SPI_DMA_CH_AUTO;
     ret = spi_bus_initialize(VSPI_HOST, bus_config, dma_chan);
     ESP_ERROR_CHECK(ret);
-    // Adding Eink to bus
+    // Adding display to bus
     spi_device_interface_config_t devcfg={
         .clock_speed_hz=2000000,
         .mode=0,
@@ -159,4 +130,5 @@ int EpdIf::IfInit(void) {
     };
     ret=spi_bus_add_device(VSPI_HOST, &devcfg, &spi);
     ESP_ERROR_CHECK(ret);
+    return 0;
 }
